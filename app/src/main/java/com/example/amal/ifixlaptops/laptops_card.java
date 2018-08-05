834ede48522c087fe94df44877681f4650a4425c
Amal Wickramarathna <wickramarathna.amal@gmail.com>
2018-08-05 09:55:01 +0530

    Initial commit

---
+++ app/src/main/java/com/example/amal/ifixlaptops/laptops_card.java
@@ -0,0 +1,13 @@
+package com.example.amal.ifixlaptops;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class laptops_card extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_laptops_card);
+    }
+}
